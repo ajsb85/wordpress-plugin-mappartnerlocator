7ebc9cdc522f84eaaa7f027d30bfbe269d9c68bc
Alexander J. Salas B <a.salas@ieee.org>
2016-03-22 11:11:51 +0100

    Assets only in post

--- mappartnerlocator.php
+++ mappartnerlocator.php
@@ -346,99 +346,11 @@ class MapPartnerLocator {
 			<input id="pac-input" class="controls" type="text" placeholder="Search Box">
 	     <div id="map"></div>
 	     <script>
-	       // This example adds a search box to a map, using the Google Place Autocomplete
-	       // feature. People can enter geographical searches. The search box will return a
-	       // pick list containing a mix of places and predicted search terms.
-
-	       // This example requires the Places library. Include the libraries=places
-	       // parameter when you first load the API. For example:
-	       // <script src="https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&libraries=places">
-
-	      //  function initAutocomplete() {
-				// <?php if ( isset ( $codex_stored_meta['meta-latitude'] ) ) { ?>
-				// 	var map = new google.maps.Map(document.getElementById('map'), {
-				// 		center: {
-				// 		 lat: <?php echo $codex_stored_meta['meta-latitude'][0]; ?>,
-				// 		 lng: <?php echo $codex_stored_meta['meta-longitude'][0]; ?>
-				// 	 	},
-				// 		zoom: 24,
-				// 		mapTypeId: google.maps.MapTypeId.ROADMAP
-				// 	});
-				// 	var marker =	new google.maps.Marker({
-				// 			map: map,
-				// 			title: "Location",
-				// 			position: {
-				// 				lat: <?php echo $codex_stored_meta['meta-latitude'][0]; ?>,
-				// 				lng: <?php echo $codex_stored_meta['meta-longitude'][0]; ?>
-				// 			}
-				// 		});
-				// <?php } else { ?>
-				// 	var map = new google.maps.Map(document.getElementById('map'), {
-				// 		center: {lat: -33.8688, lng: 151.2195},
-	      //     zoom: 13,
-				// 		mapTypeId: google.maps.MapTypeId.ROADMAP
-				// 	});
-				// <?php } ?>
-	      //    // Create the search box and link it to the UI element.
-	      //    var input = document.getElementById('pac-input');
-	      //    var searchBox = new google.maps.places.SearchBox(input);
-	      //    map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
-				//
-	      //    // Bias the SearchBox results towards current map's viewport.
-	      //    map.addListener('bounds_changed', function() {
-	      //      searchBox.setBounds(map.getBounds());
-	      //    });
-				//
-	      //    var markers = [];
-	      //    // Listen for the event fired when the user selects a prediction and retrieve
-	      //    // more details for that place.
-	      //    searchBox.addListener('places_changed', function() {
-	      //      var places = searchBox.getPlaces();
-				//
-	      //      if (places.length == 0) {
-	      //        return;
-	      //      }
-				//
-	      //      // Clear out the old markers.
-	      //      markers.forEach(function(marker) {
-	      //        marker.setMap(null);
-	      //      });
-	      //      markers = [];
-				//
-	      //      // For each place, get the icon, name and location.
-	      //      var bounds = new google.maps.LatLngBounds();
-	      //      places.forEach(function(place) {
-	      //        var icon = {
-	      //          url: place.icon,
-	      //          size: new google.maps.Size(71, 71),
-	      //          origin: new google.maps.Point(0, 0),
-	      //          anchor: new google.maps.Point(17, 34),
-	      //          scaledSize: new google.maps.Size(25, 25)
-	      //        };
-				//
-	      //        // Create a marker for each place.
-	      //        markers.push(new google.maps.Marker({
-	      //          map: map,
-				// 				 icon: icon,
-	      //          title: place.name,
-	      //          position: place.geometry.location
-	      //        }));
-				// 			 var latitude = place.geometry.location.lat();
-				// 			 var longitude = place.geometry.location.lng();
-				// 			 document.getElementById("meta-latitude").value = latitude;
-				// 			 document.getElementById("meta-longitude").value = longitude;
-				// 			 //place.place_id
-	      //        if (place.geometry.viewport) {
-	      //          // Only geocodes have viewport.
-	      //          bounds.union(place.geometry.viewport);
-	      //        } else {
-	      //          bounds.extend(place.geometry.location);
-	      //        }
-	      //      });
-	      //      map.fitBounds(bounds);
-	      //    });
-	      //  }
-
+			 var lat, lng;
+			 <?php if ( isset ( $codex_stored_meta['meta-latitude'] ) ) { ?>
+				 lat = <?php echo $codex_stored_meta['meta-latitude'][0]; ?>;
+				 lng = <?php echo $codex_stored_meta['meta-longitude'][0]; ?>;
+			<?php } ?>
 	     </script>
 
 			<table id="newmeta">
@@ -552,8 +464,8 @@ class MapPartnerLocator {
 		 * Adds the meta box stylesheet when appropriate
 		 */
 		function codex_admin_assets(){
-			global $typenow;
-			if( $typenow == 'partner' ) {
+			global $pagenow, $typenow;
+			if(is_admin() && $pagenow=='post-new.php' OR $pagenow=='post.php' && $typenow == 'partner' ) {
 				wp_enqueue_style( 'codex_meta_box_styles', plugin_dir_url( __FILE__ ) . 'assets/style.css' );
 				wp_enqueue_script( 'main-plugin-js', plugin_dir_url( __FILE__ ) . 'assets/main.js', null , '0.1.0', false );
 				wp_enqueue_script( 'google-map-js', '//maps.googleapis.com/maps/api/js?key=AIzaSyAJoj6C6lAUNU_t8rK9MxdDFz3ZPh8LhmQ&libraries=places&callback=initAutocomplete',
