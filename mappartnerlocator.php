cdd5aae1b7fdc212bb16d1c7ada1206839215f99
Alexander J. Salas B <a.salas@ieee.org>
2016-03-26 02:27:29 +0100

    Added attach icon for markers

--- mappartnerlocator.php
+++ mappartnerlocator.php
@@ -83,6 +83,13 @@ class options_page {
 		if ( !current_user_can( 'manage_options' ) )  {
         wp_die( 'You do not have sufficient permissions to access this page.' );
     }
+		// Save attachment ID
+		if ( isset( $_POST['submit_image_selector'] ) && isset( $_POST['image_attachment_id'] ) ) :
+			update_option( 'media_selector_attachment_id', absint( $_POST['image_attachment_id'] ) );
+		endif;
+
+		wp_enqueue_media();
+
 	?>
 
 	    <div>
@@ -92,16 +99,33 @@ class options_page {
 	        <form method="post" action="options.php" novalidate="novalidate">
 	            <?php wp_nonce_field('update-options') ?>
 							<table class="form-table">
-							<tbody><tr>
-							<th scope="row"><label for="google_map_api_key">Google Map API Key</label></th>
-							<td><input type="text" name="google_map_api_key" value="<?php echo get_option('google_map_api_key'); ?>" class="regular-text"/></td>
-							</tr>
-								</tbody></table>
+								<tbody>
+									<tr>
+										<th scope="row">
+											<label for="google_map_api_key">Google Map API Key</label>
+										</th>
+										<td>
+											<input type="text" name="google_map_api_key" value="<?php echo get_option('google_map_api_key'); ?>" class="regular-text"/>
+										</td>
+									</tr>
+									<tr>
+										<th scope="row">
+											<input id="upload_image_button" type="button" class="button" value="<?php _e( 'Upload image' ); ?>" />
+										</th>
+										<td>
+											<div class='image-preview-wrapper'>
+												<img id='image-preview' src='<?php echo wp_get_attachment_url( get_option( 'media_selector_attachment_id' ) ); ?>' height='100'/>
+											</div>
+										</td>
+									</tr>
+								</tbody>
+							</table>
 	            <?php
 	            submit_button();
 	            ?>
 							<input type="hidden" name="action" value="update" />
 							<input type="hidden" name="page_options" value="google_map_api_key" />
+							<input type='hidden' name='image_attachment_id' id='image_attachment_id' value='<?php echo get_option( 'media_selector_attachment_id' ); ?>' />
 	        </form>
 	    </div>
 	<?php
@@ -597,7 +621,76 @@ class MapPartnerLocator {
 
 		add_action( 'wp_ajax_partners', 'partners_callback' );
 
+		function media_selector_scripts() {
+
+			$my_saved_attachment_post_id = get_option( 'media_selector_attachment_id', 0 );
+
+			?><script type='text/javascript'>
+
+				jQuery( document ).ready( function( $ ) {
+
+					// Uploading files
+					var file_frame;
+					var wp_media_post_id = wp.media.model.settings.post.id; // Store the old id
+					var set_to_post_id = <?php echo $my_saved_attachment_post_id; ?>; // Set this
+
+					jQuery('#upload_image_button').on('click', function( event ){
+
+						event.preventDefault();
+
+						// If the media frame already exists, reopen it.
+						if ( file_frame ) {
+							// Set the post ID to what we want
+							file_frame.uploader.uploader.param( 'post_id', set_to_post_id );
+							// Open frame
+							file_frame.open();
+							return;
+						} else {
+							// Set the wp.media post id so the uploader grabs the ID we want when initialised
+							wp.media.model.settings.post.id = set_to_post_id;
+						}
+
+						// Create the media frame.
+						file_frame = wp.media.frames.file_frame = wp.media({
+							title: 'Select a image to upload',
+							button: {
+								text: 'Use this image',
+							},
+							multiple: false	// Set to true to allow multiple files to be selected
+						});
+
+						// When an image is selected, run a callback.
+						file_frame.on( 'select', function() {
+							// We set multiple to false so only get one image from the uploader
+							attachment = file_frame.state().get('selection').first().toJSON();
+
+							// Do something with attachment.id and/or attachment.url here
+							$( '#image-preview' ).attr( 'src', attachment.url ).css( 'width', 'auto' );
+							$( '#image_attachment_id' ).val( attachment.id );
+
+							// Restore the main post ID
+							wp.media.model.settings.post.id = wp_media_post_id;
+						});
+
+							// Finally, open the modal
+							file_frame.open();
+					});
+
+					// Restore the main ID when the add media button is pressed
+					jQuery( 'a.add_media' ).on( 'click', function() {
+						wp.media.model.settings.post.id = wp_media_post_id;
+					});
+				});
+
+			</script><?php
+
+		}
+
+		add_action('admin_footer', 'media_selector_scripts');
+
 		new options_page;
+
+
 
 		// Add settings link on plugin page
 		function codex_settings_link($links) {
